88ba1d63ef71ce134390dce27a0ea19b282c6e77
SiwakornJew <69206848+SiwakornJew@users.noreply.github.com>
2022-04-25 07:22:47 +0700

    addcashoutTable

--- controller/admin/cart.controller.js
+++ controller/admin/cart.controller.js
@@ -63,8 +63,8 @@ module.exports = {
       const thisCart = await Cart.findById(table.cart);
 
       thisCart.detail.map((e) => {
-        _price = e.quantity * e.price;
-        totalPrice += _price;
+        //_price = e.quantity * e.price;
+        totalPrice += e.price;
       });
 
       if (thisCart.detail.length === 0) {
